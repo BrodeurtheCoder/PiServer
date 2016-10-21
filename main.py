c5bfdad090a2cd0de6db8526bd05fcf1aa9c5a7f
Lia Brodeur <brodeur.lia@gmail.com>
2016-10-20 22:00:31 -0400

    Added random number generation for testing/demo

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import socket, sys, time    # Import socket module
+from random import randint
 
 s = socket.socket()
 host = "104.131.22.46"
 port = 2000
+testMessage = ""
 
-testMessage = "Hello world!"
+for i in range(16):
+	testMessage += str(randint(0,9))
+
 
 s.connect((host, port))
 
