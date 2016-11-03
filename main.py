193bd832f5f3b8035da43f5288e4d8047755ea2f
Lia Brodeur <brodeur.lia@gmail.com>
2016-11-02 22:33:15 -0400

    added in code for the real time clock

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
-import socket, sys, time    # Import socket module
+import socket, sys, time #datetime, SDL_DS32131    # Import socket module
 from random import randint
 
 host = "104.131.22.46"
 port = 2000
 
-time.sleep(25)
+time.sleep(25)		#need to get rid of this at some point
+
+#startTime = datetime.datetime.utcnow()
+#ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
+#ds3231.write_now()
+
+
+#server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+#logging.basicConfig(Level = logging.DEBUG,
+#	format='(%(threadName)-5s) %(message)s', )
+
+###UPDATE CODE FROM BUFFER.PY TO THIS TO INTEGRATE ALL THE CODE###
+
 
 while True:
-	testMessage = ""
 
-	for i in range(16):
+	#testTimeMessage = [0,0]
+	#currentTime = datetime.datetime.utcnow()
+	#testTimeMessage[0] = currentTime
+	testMessage = ""
+	for i in range(8):
 		testMessage += str(randint(0,9))
+
+	#testTimeMessage[1] = testMessage
 
 	# Networking
 	s = socket.socket() 				# Creates n
 
 	# TODO: Implement failure check for connection failing
 	s.connect((host, port))				# Connects to server
+
+	#print "current time: \t" + currentTime
+	#print "testMessage: %s" %testMessage
+	#print "Sending testTimeMessage: " + testTimeMessage
+	#s.send(testTimeMessage)
+	#print "Sent message successfully!"
+	#s.close()
+	#print "Closing connection to server \n"
+	#time.sleep(5)
 
 	print "Sending message %s" %testMessage
 	s.send(testMessage)					# Sends message to server
@@ -25,3 +51,5 @@ while True:
 	print "Closing connection to server \n"
 	time.sleep(5)						# Sleeps for n seconds before 
 										# sending again
+
+
---
+++ realTimeControl.py
@@ -0,0 +1,29 @@
+# real time clock control code for pi 3
+# needs to initialize current time, send to all the nodes via bluetooth
+# then check time of each of the data packest sent from nodes
+
+import sys, time, datetime, SDL_DS3231
+
+for i in range(16):
+	testMessage += str(randint(0,9))
+
+print ""
+print "Testing code for real time control"
+print ""
+print "starting time for system:" + time.strftime(“%Y-%m-%d %H:%M:%S”)
+
+startTime = datetime.datetime.utcnow()
+ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
+ds3231.write_now()
+
+while True:
+
+	currentTime = datetime.datetime.utcnow()
+	changeInTime = currentTime - startTime
+
+	print ""
+	print "Pi time: \t" + time.strftime(“%Y-%m-%d %H:%M:%S”)
+	print "DS3231 time: \t" + time.strftime(“%Y-%m-%d %H:%M:%S”)
+	print "current time: ", currentTime
+	print "change in time: ", changeInTime
+	time.sleep(10)
