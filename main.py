e09a68931cf4f7dab100c2e763d8f1325ab53145
Lia Brodeur <brodeur.lia@gmail.com>
2016-11-03 16:31:43 -0400

    updated local server code with bluetooth/threading buffer code

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import socket, sys, time #datetime, SDL_DS32131    # Import socket module
 from random import randint
 
-host = "104.131.22.46"
-port = 2000
+import bluetooth, threading, logging
 
-time.sleep(25)		#need to get rid of this at some point
+host = "104.131.22.46"
+portWi = 2000
+
+def handler():
+	print "Thread started for", child_node
+	while True:
+		data = client_sock.recv(1024)
+		print "Controller node received", data, "from", child_node
+
+		if data == "quit"
+			return
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+portBlue = 0x1001
+thread = []
+
+server_sock.bind(("",portBlue))
+server_sock.listen(1)
+print "Server listening..."
+
+time.sleep(10)		#need to get rid of this at some point
 
 #startTime = datetime.datetime.utcnow()
 #ds3231 = SDL_DS3231.SDL_DS3231(1, 0x68)
 #ds3231.write_now()
 
-
-#server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
 #logging.basicConfig(Level = logging.DEBUG,
 #	format='(%(threadName)-5s) %(message)s', )
-
 ###UPDATE CODE FROM BUFFER.PY TO THIS TO INTEGRATE ALL THE CODE###
 
 
@@ -27,13 +43,32 @@ while True:
 	for i in range(8):
 		testMessage += str(randint(0,9))
 
+	client_sock, child_address = server_sock.accept()
+	child_node = bluetooth.lookup_name(child_address[0])
+	print "Server accepted connection from", child_node
+
+	t = threading.Thread(name = child_node, target = handler)
+	t.setDaemon(True)
+	t.start()
+	print "Thread started..."
+	main_thread = threading.currentThread()
+
+	for j in threading.enumerate():
+        if j is not main_thread:
+            print "Joining thread..."
+            j.join()
+            logging.debug("joined ", j.getName())
+        if not threading.enumerate():
+            break
+
+
 	#testTimeMessage[1] = testMessage
 
 	# Networking
 	s = socket.socket() 				# Creates n
 
 	# TODO: Implement failure check for connection failing
-	s.connect((host, port))				# Connects to server
+	s.connect((host, portWi))				# Connects to server
 
 	#print "current time: \t" + currentTime
 	#print "testMessage: %s" %testMessage
